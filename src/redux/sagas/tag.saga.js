ad5cfc0bc82f6e41e6a990d74d2c97e4c1bfacb4
Anniessa Antar <anniessa.antar@gmail.com>
2023-03-13 16:06:07 -0500

    tag saga set up

--- src/redux/sagas/tag.saga.js
+++ src/redux/sagas/tag.saga.js
@@ -2,13 +2,17 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 //get tags
-function* getTags(action) {
+function* getTag() {
     try {
         let response = yield axios.get('/api/tag');
         yield put ({type: 'SET_TAG', payload: response.data})
     } catch (error) {
         console.error(`Error getting tag`, error);
     }
+}
+
+function* tagSaga() {
+    yield takeLatest('GET_TAG', getTag);
 }
 
 export default tagSaga;
