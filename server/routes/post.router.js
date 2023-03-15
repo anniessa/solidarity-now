18d6aa85f71e16a6c3d9e76df76268465da80742
Anniessa Antar <anniessa.antar@gmail.com>
2023-03-15 11:24:14 -0500

    post.router now captures the tag_ids and stores them in the database

--- server/routes/post.router.js
+++ server/routes/post.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-const {rejectUnauthenticated} = require('../modules/authentication-middleware');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * GET route template
@@ -10,74 +10,67 @@ router.get('/', (req, res) => {
   // GET route code here
   const sqlText = `SELECT * from "posts"`
   pool.query(sqlText)
-  .then(result => {
-    res.send(result.rows);
-  })
-  .catch(error => {
-    console.log(`Error getting all the posts`, error)
-    res.sendStatus(500);
-  })
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log(`Error getting all the posts`, error)
+      res.sendStatus(500);
+    })
 });
 
 // getting all the tags for each post - used string_agg to combine all the tags
 router.get('/:id', (req, res) => {
- const sqlText = 
- `
+  const sqlText =
+    `
  SELECT "posts".post_type, "posts".content, "posts".additional_resource, string_agg("tags".tag_name, ', ') AS "tags_column" FROM "tags"
  JOIN "tags_posts" ON "tags".id = "tags_posts".tags_id
  JOIN "posts" ON "posts".id = "tags_posts".posts_id
  WHERE "posts".id = 8
  GROUP BY "posts".post_type, "posts".content, "posts".additional_resource; 
  `
- pool.query(sqlText, [req.params.id])
- console.log('tag id from req.params', req.params.id)
- .then(result => {
-  res.send(result.rows);
- })
- .catch(err => {
-  console.log(`Error getting post by ID`, err)
-  res.sendStatus(500)
- })
+  pool.query(sqlText, [req.params.id])
+  console.log('tag id from req.params', req.params.id)
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => {
+      console.log(`Error getting post by ID`, err)
+      res.sendStatus(500)
+    })
 })
 
 
-router.post('/', rejectUnauthenticated, (req, res) => {
+router.post('/', rejectUnauthenticated, async (req, res) => {
   // POST route code here
   console.log(req.user)
   console.log('req.body:', req.body)
+  let tag_ids = req.body.tag_ids
 
-  //RETURNING 'id' will give us back the id of the created post
-  const insertPostQuery = `
-  INSERT INTO "posts" ("post_type", "content", "additional_resource", "user_id")
-  VALUES ($1, $2, $3, $4)
-  RETURNING "id";`
+  try {
+    const insertPostQuery = `
+    INSERT INTO "posts" ("post_type", "content", "additional_resource", "user_id")
+    VALUES ($1, $2, $3, $4)
+    RETURNING "id";`
+     //RETURNING 'id' will give us back the id of the created post
+    result = await pool.query(insertPostQuery, [req.body.post_type, req.body.content, req.body.additional_resource, req.body.user_id])
+    createdPostId = result.rows[0].id
 
-  //first query makes the post
-  pool.query(insertPostQuery, [req.body.post_type, req.body.content, req.body.additional_resource, req.body.user_id])
-  .then(result => {
-    console.log('New post id:', result.rows[0].id) //here is the ID
-
-    let createdPostId = result.rows[0].id
-    //this constant makes the tag reference
+    // mapping over the tag_ids array to do the query for EACH individual tag
+    const postTags = tag_ids.map(tagId => {
     const insertPostTagQuery = `
     INSERT INTO "tags_posts" ("posts_id", "tags_id")
     VALUES ($1, $2);
     `
-    console.log(req.body);
-    //need to loop over the array of tag_ids and send tag_ids[n] as the req.body
-    
-    pool.query(insertPostTagQuery, [createdPostId, req.body.tag_ids])
-     //this query associates the tag to the post
-    .then(result => {
-        //now that both are done, send back success
-        console.log('tags_id:', req.body.tag_ids)
-        res.sendStatus(201);
-      })
-      .catch(err => {
-        console.log(`Error posting post with tag`, err)
-        res.sendStatus(500);
-      })
-  });
-});
+    pool.query(insertPostTagQuery, [createdPostId, tagId])
+    })
+    // returning an empty object, until the map is completed and once its completed, it sends status
+    Promise.all(postTags)
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(`Error posting post with tag`, err)
+    res.sendStatus(500);  
+  }
+    });
 
 module.exports = router;
